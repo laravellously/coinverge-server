ed87352ca6a25232381e4bef30ef37180aa575b5
Martins 'Laravellous Michael <martins.michael71@gmail.com>
2021-07-30 02:50:51 +0100

    Update redis.js plugin

--- plugins/redis.js
+++ plugins/redis.js
@@ -1,21 +1,25 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const Redis = require('fastify-redis')
-
-module.exports = fp(async (fastify, opts) => {
-    // Full options documentation here https://github.com/luin/ioredis/blob/master/API.md#Redis
-
-    let redisOpts = Object.assign({}, {
-        options: {
-            host: process.env.REDIS_HOST || '127.0.0.1',
-            port: process.env.REDIS_PORT || '6379',
-            // password: process.env.REDIS_PASSWORD || '',
-            // db: process.env.REDIS_DB || {{ redis_db }}
-            connectTimeout: 500,
-            maxRetriesPerRequest: 1
-    }
-  }, opts.redis)
-
-fastify.register(Redis, redisOpts)
+module.exports = fp(async function (fastify, opts) {
+  fastify.register(require('fastify-redis'))
 })
+
+// const Redis = require('fastify-redis')
+
+// module.exports = fp(async (fastify, opts) => {
+//     // Full options documentation here https://github.com/luin/ioredis/blob/master/API.md#Redis
+
+//     let redisOpts = Object.assign({}, {
+//         options: {
+//             host: process.env.REDIS_HOST || '127.0.0.1',
+//             port: process.env.REDIS_PORT || '6379',
+//             // password: process.env.REDIS_PASSWORD || '',
+//             // db: process.env.REDIS_DB || {{ redis_db }}
+//             connectTimeout: 500,
+//             maxRetriesPerRequest: 1
+//     }
+//   }, opts.redis)
+
+// fastify.register(Redis, redisOpts)
+// })
